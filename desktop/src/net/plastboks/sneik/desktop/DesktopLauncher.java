f81903adb1416cd3a15bbcc61c4096bf1a2b2066
Alexander Skjolden <alex@plastboks.net>
2014-12-12 13:07:52 +0100

    first working commit
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

---
+++ desktop/src/net/plastboks/sneik/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.plastboks.sneik.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.plastboks.sneik.SneikGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SneikGame(), config);
+	}
+}
