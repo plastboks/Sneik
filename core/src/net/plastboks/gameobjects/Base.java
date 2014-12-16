fdd08f499c27c2447e920a7db40a477d23fbc567
Alexander Skjolden <alex@plastboks.net>
2014-12-16 20:31:03 +0100

    base class for game objects {snake, bird, mouse} and basic implementation of bird and mouse
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

---
+++ core/src/net/plastboks/gameobjects/Base.java
@@ -0,0 +1,34 @@
+package net.plastboks.gameobjects;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by alex on 12/16/14.
+ */
+public abstract class Base {
+
+    private Vector2 pos;
+    private int width;
+    private int height;
+    private int gameHeight;
+    private boolean isAlive;
+
+    public Base(float x, float y, int width, int height) {
+        this.pos = new Vector2(x, y);
+        this.width = width;
+        this.height = height;
+
+        isAlive = true;
+    }
+
+    public void incrementX() { pos.x++; }
+    public void incrementY() { pos.y++; }
+    public float getX() { return pos.x; }
+    public float getY() { return pos.y; }
+    public int getWidth() { return width; }
+    public int getHeight() { return height; }
+
+    public int getRotation() { return 0; }
+
+    public abstract void update(float delta);
+}
--- core/src/net/plastboks/gameobjects/Mouse.java
+++ core/src/net/plastboks/gameobjects/Mouse.java
@@ -3,5 +3,13 @@ package net.plastboks.gameobjects;
 /**
  * Created by alex on 12/15/14.
  */
-public class Mouse {
+public class Mouse extends Base {
+
+    public Mouse(float x, float y, int width, int height, int gameHeight) {
+        super(x, y, width, height);
+    }
+
+    public void update(float delta) {
+        incrementX();
+    }
 }
