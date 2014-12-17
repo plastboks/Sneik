b45242233e6a51ec81fd538cb23623b6b4cf5d34
Alexander Skjolden <alex@plastboks.net>
2014-12-17 12:46:44 +0100

    extended the bird and mouse class with random movements
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- core/src/net/plastboks/gameobjects/Bird.java
+++ core/src/net/plastboks/gameobjects/Bird.java
@@ -1,15 +1,46 @@
 package net.plastboks.gameobjects;
 
+import net.plastboks.shared.Directions;
+
+import java.util.Random;
+
 /**
  * Created by alex on 12/15/14.
  */
-public class Bird extends Base{
+public class Bird extends Creature implements Artificial
+{
+
+    Random r;
 
     public Bird(float x, float y, int width, int height, int gameHeight) {
         super(x, y, width, height);
+        r = new Random();
+    }
+
+    public void changeDir() {
+        int factor = 1000;
+        int divider = 97;
+
+        switch (r.nextInt(factor) % divider) {
+            case 0:
+                setDir(Directions.NORTH);
+                break;
+            case 1:
+                setDir(Directions.EAST);
+                break;
+            case 2:
+                setDir(Directions.SOUTH);
+                break;
+            case 3:
+                setDir(Directions.WEST);
+                break;
+
+        }
     }
 
     public void update(float delta) {
-        incrementX();
+        float lvl = 0.4f;
+        move(delta + lvl);
+        changeDir();
     }
 }
--- core/src/net/plastboks/gameobjects/Mouse.java
+++ core/src/net/plastboks/gameobjects/Mouse.java
@@ -1,15 +1,46 @@
 package net.plastboks.gameobjects;
 
+import net.plastboks.shared.Directions;
+
+import java.util.Random;
+
 /**
  * Created by alex on 12/15/14.
  */
-public class Mouse extends Base {
+public class Mouse extends Creature implements Artificial
+{
+
+    private Random r;
 
     public Mouse(float x, float y, int width, int height, int gameHeight) {
         super(x, y, width, height);
+        r = new Random();
     }
 
     public void update(float delta) {
-        incrementX();
+        float lvl = 0.5f;
+        move(delta + lvl);
+        changeDir();
+    }
+
+    public void changeDir() {
+        int factor = 1000;
+        int divider = 97;
+
+        switch (r.nextInt(factor) % divider) {
+            case 0:
+                setDir(Directions.NORTH);
+                break;
+            case 1:
+                setDir(Directions.EAST);
+                break;
+            case 2:
+                setDir(Directions.SOUTH);
+                break;
+            case 3:
+                setDir(Directions.WEST);
+                break;
+
+        }
     }
 }
