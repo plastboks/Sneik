c4ece1d8fba919b32f442ad9a63c242b9148ce8e
Alexander Skjolden <alex@plastboks.net>
2014-12-17 12:44:47 +0100

    branched out the inner Node class from Creature to seperate class file
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

---
+++ core/src/net/plastboks/gameobjects/Node.java
@@ -0,0 +1,35 @@
+package net.plastboks.gameobjects;
+
+import com.badlogic.gdx.math.Vector2;
+import net.plastboks.screens.GameScreen;
+import net.plastboks.shared.Directions;
+
+/**
+ * Created by alex on 12/17/14.
+ */
+
+public class Node {
+    public Vector2 v;
+    public Directions dir;
+
+    public Node(Vector2 v, Directions d) {
+        this.v = v;
+        this.dir = d;
+    }
+
+    public float getX() { return v.x; }
+    public float getY() { return v.y; }
+    public Directions getDir() { return dir; }
+    public void setDir(Directions d) { dir = d; }
+    public void setX(float x) {
+        v.x = x;
+        if (v.x > GameScreen.GAME_WIDTH) { v.x = 0; }
+        if (v.x < 0) { v.x = GameScreen.GAME_WIDTH; }
+    }
+    public void setY(float y) {
+        v.y = y;
+        if (v.y > GameScreen.getHeight()) { v.y = 0; }
+        if (v.y < 0) { v.y = GameScreen.getHeight(); }
+    }
+}
+
