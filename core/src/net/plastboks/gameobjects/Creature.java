70f886e711e14561c5c52801ab762ad4ba17a871
Alexander Skjolden <alex@plastboks.net>
2014-12-17 12:47:24 +0100

    merged some functions and encapsulated some method for a more monotone access
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- core/src/net/plastboks/gameobjects/Creature.java
+++ core/src/net/plastboks/gameobjects/Creature.java
@@ -1,34 +1,72 @@
 package net.plastboks.gameobjects;
 
 import com.badlogic.gdx.math.Vector2;
+import net.plastboks.shared.Directions;
 
 /**
  * Created by alex on 12/16/14.
  */
 public abstract class Creature {
 
-    private Vector2 pos;
+    private Node head;
     private int width;
     private int height;
     private int gameHeight;
     private boolean isAlive;
+    private float rotation;
+
 
     public Creature(float x, float y, int width, int height) {
-        this.pos = new Vector2(x, y);
+        head = new Node(new Vector2(x, y), Directions.NORTH);
         this.width = width;
         this.height = height;
 
         isAlive = true;
     }
 
-    public void incrementX() { pos.x++; }
-    public void incrementY() { pos.y++; }
-    public float getX() { return pos.x; }
-    public float getY() { return pos.y; }
+    public void move(float delta) {
+        switch (head.getDir()) {
+            case NORTH:
+                head.setY(head.getY() - delta);
+                break;
+            case EAST:
+                head.setX(head.getX() + delta);
+                break;
+            case SOUTH:
+                head.setY(head.getY() + delta);
+                break;
+            case WEST:
+                head.setX(head.getX() - delta);
+                break;
+        }
+    }
+
+    public float getX() { return head.getX(); }
+    public float getY() { return head.getY(); }
+    public Directions getDir() { return head.getDir(); }
+    public void setX(float x) { head.setX(x); }
+    public void setY(float y) { head.setY(y); }
+    public void setDir(Directions d) { head.setDir(d); }
+
     public int getWidth() { return width; }
     public int getHeight() { return height; }
 
-    public int getRotation() { return 0; }
+    public void setGameHeight(int height) { this.gameHeight = height; }
+
+    public void setAlive(boolean b) { isAlive = b; }
+    public boolean isAlive()  { return isAlive; }
+    public void die() { setAlive(false);}
+
+    public void setRotation(Directions d) { rotation = getRotation(d); }
+    public static float getRotation(Directions d) {
+        switch(d) {
+            case NORTH: return -90;
+            case SOUTH: return 90;
+            case WEST: return 180;
+            case EAST: return 0;
+            default: return 0;
+        }
+    }
 
     public abstract void update(float delta);
 }
