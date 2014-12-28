ac435f713f9a6db2aefd5afbbb9dcc9506dac03d
Alexander Skjolden <alex@plastboks.net>
2014-12-28 13:23:30 +0100

    added lock mechanism for locking the linkedlist when modifying it and snake eat operations
    Signed-off-by: Alexander Skjolden <alex@plastboks.net>

--- core/src/net/plastboks/gameworld/GamePlay.java
+++ core/src/net/plastboks/gameworld/GamePlay.java
@@ -15,6 +15,7 @@ public class GamePlay {
     private LinkedList<Autonomous> food;
     private Snake snake;
     private int inc;
+    private boolean lock;
 
     public GamePlay(Snake snake) {
         food = new LinkedList<Autonomous>();
@@ -24,22 +25,30 @@ public class GamePlay {
     }
 
     private void initFood() {
-        add(new Bird(15, 15));
-        add(new Mouse(15, 15));
+        food.add(new Bird(15, 15));
+        food.add(new Mouse(15, 15));
     }
 
     private void remove(Autonomous a) { food.remove(a); }
     private void removeRandom() {
 
     }
-    private void add(Autonomous a) { food.add(a); }
 
-    public void increment() {
-        inc++;
-        snake.eat();
-        //if (inc % 5 == 0) { add(new Bird(15, 15)); }
-        //if (inc % 7 == 0) { add(new Mouse(15, 15)); }
+    private void addFood()
+    {
+        lock = true;
+        //if (inc % 5 == 0) { food.add(new Bird(15, 15)); }
+        //if (inc % 7 == 0) { food.add(new Mouse(15, 15)); }
+        lock = false;
     }
 
+    public void increment(Autonomous a) {
+        inc++;
+        snake.incrementBodySizeBy(4);
+        if (++inc % 5 == 0) { snake.incrementSpeed(); }
+        addFood();
+    }
+
+    public boolean getLock() { return lock; }
     public LinkedList<Autonomous> getFood() { return food; }
 }
